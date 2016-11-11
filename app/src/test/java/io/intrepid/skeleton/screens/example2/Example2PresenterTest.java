fc67925a9d5f29bef66febf45ee23020a97950c0
David Zou <david2@intrepid.io>
2016-11-11 16:40:04 -0500

    Extract common fields to BasePresenterTest

--- app/src/test/java/io/intrepid/skeleton/screens/example2/Example2PresenterTest.java
+++ app/src/test/java/io/intrepid/skeleton/screens/example2/Example2PresenterTest.java
@@ -5,10 +5,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 import io.intrepid.skeleton.models.IpModel;
-import io.intrepid.skeleton.rest.RestApi;
-import io.intrepid.skeleton.settings.UserSettings;
 import io.intrepid.skeleton.testutils.BasePresenterTest;
-import io.intrepid.skeleton.testutils.TestPresenterConfiguration;
 import rx.Observable;
 
 import static org.mockito.Mockito.verify;
@@ -19,15 +16,8 @@ public class Example2PresenterTest extends BasePresenterTest<Example2Presenter> 
     @Mock
     Example2Contract.View mockView;
 
-    private TestPresenterConfiguration testConfiguration;
-    private RestApi mockApi;
-    private UserSettings mockSettings;
-
     @Before
     public void setUp() {
-        testConfiguration = TestPresenterConfiguration.createTestConfiguration();
-        mockApi = testConfiguration.getRestApi();
-        mockSettings = testConfiguration.getUserSettings();
         presenter = new Example2Presenter(mockView, testConfiguration);
     }
 
@@ -38,20 +28,20 @@ public class Example2PresenterTest extends BasePresenterTest<Example2Presenter> 
 
         IpModel mockIpModel = new IpModel();
         mockIpModel.ip = mockIp;
-        when(mockApi.getMyIp()).thenReturn(Observable.just(mockIpModel));
-        when(mockSettings.getLastIp()).thenReturn(mockPreviousIp);
+        when(mockRestApi.getMyIp()).thenReturn(Observable.just(mockIpModel));
+        when(mockUserSettings.getLastIp()).thenReturn(mockPreviousIp);
 
         presenter.onViewCreated();
         verify(mockView).showPreviousIpAddress(mockPreviousIp);
         testConfiguration.triggerRxSchedulers();
         verify(mockView).showCurrentIpAddress(mockIp);
-        verify(mockSettings).setLastIp(mockIp);
+        verify(mockUserSettings).setLastIp(mockIp);
     }
 
     @Test
     public void testOnViewCreated_NoPreviousIp() throws Exception {
-        when(mockApi.getMyIp()).thenReturn(Observable.empty());
-        when(mockSettings.getLastIp()).thenReturn(null);
+        when(mockRestApi.getMyIp()).thenReturn(Observable.empty());
+        when(mockUserSettings.getLastIp()).thenReturn(null);
 
         presenter.onViewCreated();
         verify(mockView).hidePreviousIpAddress();
--- app/src/test/java/io/intrepid/skeleton/testutils/BasePresenterTest.java
+++ app/src/test/java/io/intrepid/skeleton/testutils/BasePresenterTest.java
@@ -1,14 +1,32 @@
 package io.intrepid.skeleton.testutils;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import io.intrepid.skeleton.base.BasePresenter;
+import io.intrepid.skeleton.rest.RestApi;
+import io.intrepid.skeleton.settings.UserSettings;
+import rx.schedulers.TestScheduler;
 
 public class BasePresenterTest<P extends BasePresenter> {
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     protected P presenter;
+    protected TestPresenterConfiguration testConfiguration;
+    protected TestScheduler ioScheduler;
+    protected TestScheduler uiScheduler;
+    protected RestApi mockRestApi;
+    protected UserSettings mockUserSettings;
+
+    @Before
+    public void baseSetup() {
+        testConfiguration = TestPresenterConfiguration.createTestConfiguration();
+        ioScheduler = testConfiguration.getIoScheduler();
+        uiScheduler = testConfiguration.getUiScheduler();
+        mockRestApi = testConfiguration.getRestApi();
+        mockUserSettings = testConfiguration.getUserSettings();
+    }
 }
